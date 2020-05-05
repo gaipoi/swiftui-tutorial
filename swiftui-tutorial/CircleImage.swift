67869682af5b0dc19733b2473fe532a1285e9605
gaipoi <gaikiday@gmail.com>
2020-05-05 19:28:07 +0900

    add: swiftUI Tutorial

---
+++ swiftui-tutorial/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  swiftui-tutorial
+//
+//  Created by Gaiki Ito on 2020/05/05.
+//  Copyright © 2020 Gaiki Ito. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ swiftui-tutorial/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  swiftui-tutorial
+//
+//  Created by Gaiki Ito on 2020/05/05.
+//  Copyright © 2020 Gaiki Ito. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
