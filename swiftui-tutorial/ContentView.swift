99052e6352a80598b3cccc535821ddbc6afe46a0
gaipoi <gaikiday@gmail.com>
2020-05-05 13:41:48 +0900

    add: initial commit

---
+++ swiftui-tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-tutorial
+//
+//  Created by Gaiki Ito on 2020/05/05.
+//  Copyright © 2020 Gaiki Ito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
