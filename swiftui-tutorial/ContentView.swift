73360bddd7c48769b312fb96f3e944e049bf8462
gaipoi <gaikiday@gmail.com>
2020-05-06 11:58:59 +0900

    add: swiftUI tutorial leak of commit

--- swiftui-tutorial/ContentView.swift
+++ swiftui-tutorial/ContentView.swift
@@ -10,7 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    // 特に値を指定しなくても、親Viewのコンテンツ含むスペースを自動展開してくれる
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            Spacer()
+        }
     }
 }
 
--- swiftui-tutorial/MapView.swift
+++ swiftui-tutorial/MapView.swift
@@ -7,10 +7,19 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MapView: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinete = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinete, span: span)
+        uiView.setRegion(region, animated: true)
     }
 }
 
@@ -19,3 +28,4 @@ struct MapView_Previews: PreviewProvider {
         MapView()
     }
 }
+
